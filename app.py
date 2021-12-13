411e13e6de87f5c197056ec922e503835dfc9773
Cravenius <89584416+Cravenius@users.noreply.github.com>
2021-12-13 17:06:51 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -19,22 +19,13 @@ def predict():
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
 
-    age_form = request.form['age'].value
-    creatinine_phosphokinase_form = request.form['creatinine_phosphokinase'].value
-    ejection_fraction_form = request.form['ejection_fraction'].value
-    serum_creatinine_form = request.form['serum_creatinine'].value
-    time_form = request.form['time'].value
-    status_form = ""
-
     result = ""
     if prediction == 1:
-      status_form = "Meninggal"
       result = 'Berdasarkan record tersebut, orang yang bersangkutan sudah meninggal'
     else:
-      status_form = "Hidup"
       result = 'Berdasarkan record tersebut, orang yang bersangkutan tidak meninggal'
 
-    return render_template('index.html', prediction_text='{}'.format(result), age='{}'.format(age_form), creatinine_phosphokinase='{}'.format(creatinine_phosphokinase_form), ejection_fraction='{}'.format(ejection_fraction_form), serum_creatinine='{}'.format(serum_creatinine_form), time='{}'.format(time_form), status='{}'.format(status_form))
+    return render_template('index.html', prediction_text='{}'.format(result))
 
 
 if __name__ == "__main__":
