b556b9504d6f622e5e30d088f1777e407a0194ef
Cravenius <89584416+Cravenius@users.noreply.github.com>
2021-12-13 16:57:05 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from datetime import time
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
@@ -18,13 +19,22 @@ def predict():
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
 
+    age_form = request.form['age'].value
+    creatinine_phosphokinase_form = request.form['creatinine_phosphokinase'].value
+    ejection_fraction_form = request.form['ejection_fraction'].value
+    serum_creatinine_form = request.form['serum_creatinine'].value
+    time_form = request.form['time'].value
+    status_form = ""
+
     result = ""
     if prediction == 1:
+      status_form = "Meninggal"
       result = 'Berdasarkan record tersebut, orang yang bersangkutan sudah meninggal'
     else:
+      status_form = "Hidup"
       result = 'Berdasarkan record tersebut, orang yang bersangkutan tidak meninggal'
 
-    return render_template('index.html', prediction_text='{}'.format(result))
+    return render_template('index.html', prediction_text='{}'.format(result), age='{}'.format(age_form), creatinine_phosphokinase='{}'.format(creatinine_phosphokinase_form), ejection_fraction='{}'.format(ejection_fraction_form), serum_creatinine='{}'.format(serum_creatinine_form), time='{}'.format(time_form), status='{}'.format(status_form))
 
 
 if __name__ == "__main__":
